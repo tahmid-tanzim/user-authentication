a9fe92aec85ec1a027e14c1f1136f9d74e8052be
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2017-10-23 17:25:25 +0600

    ESlint rule object-curly-spacing to 'never'

--- .eslintrc.js
+++ .eslintrc.js
@@ -36,6 +36,7 @@ module.exports = {
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
     'comma-dangle': ['error', 'never'],
-    'space-before-function-paren': ['error', 'always']
+    'space-before-function-paren': ['error', 'always'],
+    'object-curly-spacing': ['error', 'never']
   }
 };
